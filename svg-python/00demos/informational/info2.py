dd6a3ad1b04b287043446343f5b6a580482cada1
Ted Tedford, Stryker <ted.tedford@stryker.com>
2023-08-17 21:02:37 +0100

    init

--- svg-python/00demos/informational/info2.py
+++ svg-python/00demos/informational/info2.py
@@ -1,24 +1,35 @@
 import os
-from pathlib import Path
+import re  # Import the regular expression module
 
-CURRENT_DIR = Path(__file__).parent
+# Paths
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 IMAGES_DIR = os.path.join(CURRENT_DIR, "../../images/mazes")
-OUTPUT_DIR = "./info_outputs"
-OUTPUT_FILENAME = "file_list.txt"
+OUTPUT_DIR = os.path.join(CURRENT_DIR, "info_outputs")
 
-
-# Ensure output directory exists
+# Ensure the output directory exists
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
 
-# Open the output file for writing
-with open(os.path.join(OUTPUT_DIR, OUTPUT_FILENAME), 'w') as output_file:
-    # Iterate over all files in the directory
-    for img_file in os.listdir(IMAGES_DIR):
-        if img_file.endswith(('.png', '.jpg', '.jpeg')):
-            # Write the filename to the output file
-            output_file.write(img_file + '\n')
-            # Print the filename to the console
-            print(img_file)
+# Filename for storing the results
+OUTPUT_FILE = os.path.join(OUTPUT_DIR, "file_list.txt")
 
-print(f"File names written to {os.path.join(OUTPUT_DIR, OUTPUT_FILENAME)}")
+# Regular expression pattern to match the temperature
+temp_pattern = re.compile(r"_c_(\d+)\.png$")
+
+with open(OUTPUT_FILE, "w") as outfile:
+    for img_file in os.listdir(IMAGES_DIR):
+        # Match the pattern to the filename
+        match = temp_pattern.search(img_file)
+
+        # If a match is found, extract the temperature
+        if match:
+            temperature = match.group(1)
+            print(f"Filename: {img_file}, Temperature: {temperature}°C")
+            outfile.write(
+                f"Filename: {img_file}, Temperature: {temperature}°C\n")
+        else:
+            print(f"Filename: {img_file}, Temperature information not found")
+            outfile.write(
+                f"Filename: {img_file}, Temperature information not found\n")
+
+print(f"File names and temperatures written to {OUTPUT_FILE}")
